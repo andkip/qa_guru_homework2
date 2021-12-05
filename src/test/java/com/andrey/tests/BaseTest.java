2394b96e5b3cfca5fffadf63507c9059bc7065bb
Andrey <dyxa1@mail.ru>
2021-12-05 16:13:59 +0300

    Добавлены owner properties

--- src/test/java/com/andrey/tests/BaseTest.java
+++ src/test/java/com/andrey/tests/BaseTest.java
@@ -1,20 +1,29 @@
 package com.andrey.tests;
 
+import com.andrey.config.CredentialsConfig;
 import com.andrey.helpers.Attach;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.selenide.AllureSelenide;
+import org.aeonbits.owner.ConfigFactory;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.openqa.selenium.remote.DesiredCapabilities;
+
+import static java.lang.String.format;
 
 public class BaseTest {
 
     @BeforeAll
     static void setUp() {
+        CredentialsConfig credentials = ConfigFactory.create(CredentialsConfig.class);
+        String login = credentials.login();
+        String password = credentials.password();
+        String url = System.getProperty("url", "selenoid.autotests.cloud/wd/hub/");
+
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
         Configuration.startMaximized = true;
-        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub/";
+        Configuration.remote = format("https://%s:%s@%s", login, password, url);
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("enableVNC", true);
